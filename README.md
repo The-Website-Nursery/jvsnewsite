bf222d2f64b2cd3d314c6241e36213d5a86c6925
Alistair Swodeck <alistair@thewebsitenursery.com>
2015-03-20 08:57:56 -0400

    JVS fitness site build modulat in boottstrap

---
+++ README.md
@@ -0,0 +1,2 @@
+# JVSFitness
+Website framework for JVS fitness site all done in Bootstrap 3 with easily modular elemennts to drop into a responsive layout
---
+++ webframeworks/js/counter.js
@@ -0,0 +1,43 @@
+(function($) {
+  "use strict";
+$(document).ready(function(){
+	function sdf_FTS(_number,_decimal,_separator)
+		{
+		var decimal=(typeof(_decimal)!='undefined')?_decimal:2;
+		var separator=(typeof(_separator)!='undefined')?_separator:'';
+		var r=parseFloat(_number)
+		var exp10=Math.pow(10,decimal);
+		r=Math.round(r*exp10)/exp10;
+		rr=Number(r).toFixed(decimal).toString().split('.');
+		b=rr[0].replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1"+separator);
+		r=(rr[1]?b+'.'+rr[1]:b);
+
+		return r;
+	}
+	setTimeout(function(){
+			$('#counter-1').text('0');
+			$('#counter-2').text('0');
+			$('#counter-3').text('0');
+			$('#counter-4').text('0');
+			setInterval(function(){											
+				var curval1=parseInt($('#counter-1').text());
+				var curval2=parseInt($('#counter-2').text());
+				var curval3=parseInt($('#counter-3').text());
+				var curval4=parseInt($('#counter-4').text());
+				if(curval1<=1343){
+					$('#counter-1').text(curval1+1);
+				}
+				if(curval2<=152){
+					$('#counter-2').text(curval2+1);
+				}
+				if(curval3<=88){
+					$('#counter-3').text(curval3+1);
+				}
+				if(curval4<=343){
+					$('#counter-4').text(curval4+1);
+				}
+			}, 2);
+			
+		}, 500);
+});
+})(jQuery);
